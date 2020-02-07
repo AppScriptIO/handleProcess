00855c003066b98523e814926b1d3a4d0b9549a3
Safi <nassarsafi@gmail.com>
2020-02-07 10:58:21 +0200

    🏗️ Build distribution code.

--- source/script.js
+++ source/script.js
@@ -1,1 +1,2 @@
-export * from './manageSubprocess.js'
+"use strict";Object.defineProperty(exports, "__esModule", { value: true });var _manageSubprocess = require("./manageSubprocess.js");Object.keys(_manageSubprocess).forEach(function (key) {if (key === "default" || key === "__esModule") return;Object.defineProperty(exports, key, { enumerable: true, get: function () {return _manageSubprocess[key];} });});
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NvdXJjZS9zY3JpcHQuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjJFQUFBIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0ICogZnJvbSAnLi9tYW5hZ2VTdWJwcm9jZXNzLmpzJ1xuIl19
